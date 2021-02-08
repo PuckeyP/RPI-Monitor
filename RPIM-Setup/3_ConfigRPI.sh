d8559aba698ac589c928b179973e72c0524006bb
PPee <puckeyp@gmail.com>
2021-02-08 15:19:24 +0100

    Create 3_ConfigRPI.sh

---
+++ RPIM-Setup/3_ConfigRPI.sh
@@ -0,0 +1,3 @@
+# Using the command line argument --config, a directory where to read the config.ini file from can be specified
+
+python3 /opt/RPi-Reporter-MQTT2HA-Daemon/ISP-RPi-mqtt-daemon.py --config /opt/RPi-Reporter-MQTT2HA-Daemon
